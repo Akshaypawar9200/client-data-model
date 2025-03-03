090cdabe9b6178b23e182d61e058d373adb46ee7
Akshay Pawar <akshaypawar9200@gmail.com>
2025-03-03 22:05:13 +0530

    [customerInsurancedata]:added customer insurance data

---
+++ sample-data/customerInsuranceData.js
@@ -0,0 +1,39 @@
+const sampleCustomerInsuranceData = [
+  {
+    id: "ddac9697-5c45-497c-9a08-5b9cd839b43c",
+    plan_name: "Health Protection",
+    purchase_date: new Date("2024-01-15"),
+    payment_mode: "Credit Card",
+    plan_expiry_date: new Date("2025-01-15"),
+    created_At: new Date(),
+    updated_At: new Date(),
+  },
+  {
+    id: "414dadf6-6c9a-4d46-b937-8b31d86e98b3",
+    plan_name: "Life Secure",
+    purchase_date: new Date("2024-02-10"),
+    payment_mode: "Bank Transfer",
+    plan_expiry_date: new Date("2025-02-10"),
+    created_At: new Date(),
+    updated_At: new Date(),
+  },
+  {
+    id: "5c87e02a-7401-44ce-bc01-9a5018e32614",
+    plan_name: "Retirement Plan",
+    purchase_date: new Date("2023-12-20"),
+    payment_mode: "Debit Card",
+    plan_expiry_date: new Date("2024-12-20"),
+    created_At: new Date(),
+    updated_At: new Date(),
+  },
+  {
+    id: "bdcb1390-8eb9-48ed-88b5-039ea2e8d73e",
+    plan_name: "Travel Insurance",
+    purchase_date: new Date("2024-05-05"),
+    payment_mode: "Cash",
+    plan_expiry_date: new Date("2024-11-05"),
+    created_At: new Date(),
+    updated_At: new Date(),
+  },
+];
+module.exports = sampleCustomerInsuranceData;
---
+++ seeders/20250303162520-demo-customerInsuranceData.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const sampleCustomerInsuranceData = require("../sample-data/customerInsuranceData");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    try {
+      await queryInterface.bulkInsert(
+        "CustomerInsuranceDatas",
+        sampleCustomerInsuranceData,
+        {}
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
